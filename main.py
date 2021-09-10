a11e5196e53a9663c5464029330c36744ffb21d1
Christopher Frige <chrisfrige@gmail.com>
2021-09-10 09:39:48 -0300

    commit inicial

--- main.py
+++ main.py
@@ -6,9 +6,8 @@ import warnings
 warnings.simplefilter("ignore")
 
 # Diretório da pasta dos arquivos a serem lidos
-path = "C:\Programacao\TrackCash\Contagem-Linhas-ML\Reportes"
+path = os.getcwd() + "/spreadsheets/"
 os.chdir(path)
-
 # Lê o arquivo e retorna qtd de linhas e colunas numa tupla
 def ler_planilha(arquivo):
     arquivo_df = pd.read_excel(arquivo)
@@ -16,13 +15,8 @@ def ler_planilha(arquivo):
 
 # Inicialização de variáveis de contagem
 linhas = 0
+colunas = 0
 qtdArquivos = 0
-colunasMenorArquivo = 0
-menorArquivo = 0
-colunasMedioArquivo = 0
-medioArquivo = 0
-colunasMaiorArquivo = 0
-maiorArquivo = 0
 
 # Tempo de início do processamento
 timeStart = time.time()
@@ -30,20 +24,10 @@ timeStart = time.time()
 for file in os.listdir():
     dados = ler_planilha(file)
 
-    linhas = linhas + dados[0]
+    # É somado +1 devido a primeira linha nao ser contada (reservado para titulo da coluna)
+    linhas = linhas + dados[0] + 1
 
-    if dados[1] == 15:
-        colunasMenorArquivo += dados[1]
-        menorArquivo += 1
-    elif dados[1] == 16:
-        colunasMedioArquivo += dados[1]
-        medioArquivo += 1
-    elif dados[1] == 24:
-        colunasMaiorArquivo += dados[1]
-        maiorArquivo += 1
-    # Caso algum arquivo com número diferente de colunas seja lido
-    else:
-        print(f"Tamanho novo: {dados[1]}")
+    colunas = colunas + dados[1]
 
     qtdArquivos += 1
 
@@ -52,7 +36,5 @@ totalTime = time.time()
 print("-"*60)
 print(f"Foram lidos {qtdArquivos} arquivos, num tempo de processamento de {totalTime - timeStart:.2f}s")
 print(f"O total de linhas desses arquivos é {linhas}.")
-print(f"Há {menorArquivo} arquivos de 15 colunas, havendo {colunasMenorArquivo} colunas!")
-print(f"Há {medioArquivo} arquivos de 16 colunas, havendo {colunasMedioArquivo} colunas!")
-print(f"Há {maiorArquivo} arquivos de 24 colunas, havendo {colunasMaiorArquivo} colunas!")
+print(f"O total de colunas desses arquivos é {colunas}.")
 print("-"*60)
